5a58717be1920bd33941bf5732d0525be9713aab
firomsa <firomsassf@gmail.com>
2024-02-22 10:18:56 +0300

    bid-3-trial

--- src/main/java/service/DBService.java
+++ src/main/java/service/DBService.java
@@ -1,17 +1,13 @@
 package service;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.DriverManager;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 public class DBService {
 	public static String DRIVER;
     public static String URL;
     public static String USER;
     public static String PASSWORD;
-    private static final String DATE_PATTERN = "yyyy-MM-dd";
     
     public static Connection openConnection() {
         try {
@@ -25,13 +21,4 @@ public class DBService {
         }
         return null;
     }   
-    
-    public static String getFormattedDate(Date date) {
-        return new SimpleDateFormat(DATE_PATTERN).format(date);
-    }
-    public static Date getFormattedDate(String date) throws ParseException{
-        SimpleDateFormat sf = new SimpleDateFormat(DATE_PATTERN);
-        java.util.Date parsedDate = sf.parse(date);
-        return new Date(parsedDate.getTime());
-    }
 }
